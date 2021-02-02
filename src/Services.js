91dd71a12f250fe3f6d162964011781d441b710f
Zenpanic <sebastien.belmo@protonmail.com>
2021-02-02 17:00:18 +0100

    services page remade

--- src/Services.js
+++ src/Services.js
@@ -3,197 +3,62 @@ import 'tachyons';
 import Logo2 from './Logo2';
 import { FormattedMessage } from 'react-intl';
 import './services.css';
+import Computer from './images/computer.png';
+import Webdev from './images/webdev.png';
 
 const Services = props => {
 
     const desc = <h1 className="logodesc"><FormattedMessage
         id="services.logo-desc"
-        defaultMessage="From the simple and efficient landing page to the complex e-commerce platform, 
-        we'll build for you the perfect online presence tailored to your needs."
+        defaultMessage="From the simple and efficient landing page to the complex e-commerce platform, we'll build for you the perfect online presence tailored to your needs."
     /></h1>
 
     return (
         <>
             <Logo2 tagline={desc} />
-            <div className="pricing">
-                <article className="pricing-text">
+
+            <article className='servicesArticle'>
+                <p><FormattedMessage
+                    id="services.p1"
+                    defaultMessage="The features and services built into your website or web app are highly dependant of your needs and requirements. Depending on 
+                    whether you want a showcase page to highlight your brand or identity, or an e-commerce platform with hundreds of products to manage and advanced user profiles, 
+                    there will be drastic differences in the approach we will take to the realization of your product."/>
+                </p>
+                <p><FormattedMessage
+                    id="services.p2"
+                    defaultMessage="In order to help you navigate through this wide ocean of possibilities, we will discuss your project in details,
+                and propose you the relevant features built with the most modern and fitted technologies, to bring your concept to life."/>
+                </p>
+                <img src={Computer} alt="computer" className="grow servicesImage" />
+                <p><FormattedMessage
+                    id="services.p3"
+                    defaultMessage="The process of understanding your project and preparing the specifications is entirely free of charge for you. We
+                will then provide you with a detailed quotation, with an estimated development time. We only start billing you from the actual
+                beginning of the development of the website or web app."/>
+                </p>
+
+                <p><FormattedMessage
+                    id="services.p4"
+                    defaultMessage="This allows you to not be worried during your decision process, and to make sure that you and us are on the same page before
+                we start working together."/>
+                </p>
+                <img src={Webdev} alt="computer" className="grow servicesImage" />
+                <p><FormattedMessage
+                    id="services.p5"
+                    defaultMessage="For the customers who have already used our services and would like us to take care of the maintenance of their product, we also provide a
+                maintenance service at the flat price of 400€ (all taxes included) which includes monthly data backups, monthly updates of the softwares powering the
+                product, as well as 6 hours of graphic or functional editing. This service is valid for a period of 12 months from the date of the payment."/>
+                </p>
+                <p><FormattedMessage
+                    id="services.p6"
+                    defaultMessage="We invite you to contact us if you have any questions about our services. We hope to count you soon among our happy customers!" />
+                </p>
+                <button className="askPrice" value="Contact" onClick={() => props.changePage('Contact')} title="Contact">
                     <FormattedMessage
-                        id="services.disclaimer"
-                        defaultMessage="Please note that those prices are here to give you a general idea of what to expect. 
-                           Contact us to get an accurate quote based on the needs and requirements of your project."
-                    />
-                </article>
-            </div>
-
-            <div className="pricing-container">
-                <div className="pricing-element">
-                    <h1 className="product-title">
-                        <FormattedMessage
-                            id="services.title-landing"
-                            defaultMessage="Landing Page" />
-                    </h1>
-                    <div className="product-pricetag">
-                        <p><span className="product-price">900€</span></p>
-                    </div>
-                    <ul>
-                        <li className="product-feature odd-li"><FormattedMessage
-                            id="services.feature-s1"
-                            defaultMessage="Elegant, fully handcrafted webapp, to make you stand out from the crowd"
-                        /></li>
-                        <li className="product-feature even-li"><FormattedMessage
-                            id="services.feature-s2"
-                            defaultMessage="Responsive design (adapted to all supports)"
-                        /></li>
-                        <li className="product-feature odd-li"><FormattedMessage
-                            id="services.feature-s3"
-                            defaultMessage="Secure online payment solution"
-                        /></li>
-                        <li className="product-feature even-li"><FormattedMessage
-                            id="services.feature-s4"
-                            defaultMessage="CMS allowing you to easily edit your content"
-                        /></li>
-                        <li className="product-feature odd-li"><FormattedMessage
-                            id="services.feature-s5"
-                            defaultMessage="Contact form linked to your mailbox"
-                        /></li>
-                        <li className="product-feature even-li"><FormattedMessage
-                            id="services.feature-s6"
-                            defaultMessage="Optimized SEO for high visibility on the search engines"
-                        /></li>
-                        <li className="product-feature odd-li"><FormattedMessage
-                            id="services.feature-s7"
-                            defaultMessage="SSL certificate: with HTTPS, you have to worry less"
-                        /></li>
-                        <li className="product-feature even-li"><FormattedMessage
-                            id="services.feature-s8"
-                            defaultMesssage="Hosting + domain name + email included for 12 months"
-                        /></li>
-                    </ul>
-                    <p className="product-description">
-                        <FormattedMessage
-                            id="services.description-landing"
-                            defaultMessage="Ideal for start-ups, freelancers and small companies willing to showcase a specific product, service or work. It can also be used as an online 
-                        business card or resume." />
-                    </p>
-                    <button className="askPrice" value="Contact" onClick={() => props.changePage('Contact')} title="Contact">
-                        <FormattedMessage
-                            id="service.askprice"
-                            defaultMessage="Contact Us!" />
-                    </button>
-
-
-                </div>
-
-                <div className="pricing-element">
-                    <h1 className="product-title">
-                        <FormattedMessage
-                            id="services.title-ecommerce"
-                            defaultMessage="E-commerce Platform" />
-                    </h1>
-                    <div className="product-pricetag">
-                        <p><span className="product-price">1400€</span></p>
-                    </div>
-                    <ul>
-                        <li className="product-feature odd-li"><FormattedMessage
-                            id="services.feature-s1"
-                            defaultMessage="Elegant, fully handcrafted webapp, to make you stand out from the crowd"
-                        /></li>
-                        <li className="product-feature even-li"><FormattedMessage
-                            id="services.feature-s2"
-                            defaultMessage="Responsive design (adapted to all supports)"
-                        /></li>
-                        <li className="product-feature odd-li"><FormattedMessage
-                            id="services.feature-s3"
-                            defaultMessage="Secure online payment solution"
-                        /></li>
-                        <li className="product-feature premium even-li"><FormattedMessage
-                            id="services.feature-p1"
-                            defaultMessage="CMS allowing you full control over your content through an admin panel"
-                        /></li>
-                        <li className="product-feature odd-li"><FormattedMessage
-                            id="services.feature-s5"
-                            defaultMessage="Contact form linked to your mailbox"
-                        /></li>
-                        <li className="product-feature even-li"><FormattedMessage
-                            id="services.feature-s6"
-                            defaultMessage="Optimized SEO for high visibility on the search engines"
-                        /></li>
-                        <li className="product-feature premium odd-li"><FormattedMessage
-                            id="services.feature-p2"
-                            defaultMessage="Intuitive navigation and searchbar"
-                        /></li>
-                        <li className="product-feature premium even-li"><FormattedMessage
-                            id="services.feature-p3"
-                            defaultMessage="User accounts"
-                        /></li>
-                        <li className="product-feature odd-li"><FormattedMessage
-                            id="services.feature-s7"
-                            defaultMessage="SSL certificate: with HTTPS, you have to worry less"
-                        /></li>
-                        <li className="product-feature premium even-li"><FormattedMessage
-                            id="services.feature-p4"
-                            defaultMessage="Dedicated database(PostgreSQL or MongoDB)"
-                        /></li>
-                        <li className="product-feature premium odd-li"><FormattedMessage
-                            id="services.feature-p5"
-                            defaultMessage="Training on the usage of your website"
-                        /></li>
-                        <li className="product-feature even-li"><FormattedMessage
-                            id="services.feature-s8"
-                            defaultMesssage="Hosting + domain name + email included for 12 months"
-                        /></li>
-                    </ul>
-                    <p className="product-description">
-                        <FormattedMessage
-                            id="services.description-ecommerce"
-                            defaultMessage="The perfect solution for companies wanting to take a big step forward and go online. Reach people everywhere, anytime." />
-                    </p>
-                    <button className="askPrice" value="Contact" onClick={() => props.changePage('Contact')} title="Contact">
-                        <FormattedMessage
-                            id="service.askprice"
-                            defaultMessage="Contact Us!" />
-                    </button>
-
-                </div>
-
-                <div className="pricing-element">
-                    <h1 className="product-title">
-                        <FormattedMessage
-                            id="services.title-maintenance"
-                            defaultMessage="1 Year Maintenance" />
-                    </h1>
-                    <div className="product-pricetag">
-                        <p><span className="product-price">400€</span></p>
-                    </div>
-
-                    <ul>
-                        <li className="product-feature odd-li"><FormattedMessage
-                            id="services.feature-m1"
-                            defaultMessage="Monthly back-up of your website and database"
-                        /></li>
-                        <li className="product-feature even-li"><FormattedMessage
-                            id="services.feature-m2"
-                            defaultMessage="Monthly updates of the technologies powering your webapp"
-                        /></li>
-                        <li className="product-feature odd-li"><FormattedMessage
-                            id="services.feature-m3"
-                            defaultMessage="6 hours of work to make esthetical and/or structural modifications"
-                        /></li>
-                    </ul>
-                    <p className="product-description">
-                        <FormattedMessage
-                            id="services.description-maintenance"
-                            defaultMessage="You're online, congratulations! Now focus on your business without any worries, knowing that your website is well taken care of through the year." />
-                    </p>
-                    <button className="askPrice" value="Contact" onClick={() => props.changePage('Contact')} title="Contact">
-                        <FormattedMessage
-                            id="service.askprice"
-                            defaultMessage="Contact Us!" />
-                    </button>
-
-                </div>
-
-            </div>
+                        id="service.askprice"
+                        defaultMessage="Contact Us!" />
+                </button>
+            </article>
         </>
     )
 }
